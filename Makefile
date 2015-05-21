4240787a90dc6ae4b9a4ed8bda397ca92955db4d
Ondřej Kupka <ondra.cap@gmail.com>
2015-05-21 13:37:06 +0200

    OAuth2

--- Makefile
+++ Makefile
@@ -6,4 +6,7 @@ run:
 
 deps.fetch:
 	go get -d -u github.com/codegangsta/negroni
+	go get -d -u github.com/goincremental/negroni-oauth2
+	go get -d -u github.com/goincremental/negroni-sessions
+	go get -d -u github.com/gorilla/mux
 	go get -d -u gopkg.in/tylerb/graceful.v1
--- server/server.go
+++ server/server.go
@@ -6,6 +6,10 @@ import (
 
 	// Vendor
 	"github.com/codegangsta/negroni"
+	oauth2 "github.com/goincremental/negroni-oauth2"
+	sessions "github.com/goincremental/negroni-sessions"
+	"github.com/goincremental/negroni-sessions/cookiestore"
+	"github.com/gorilla/mux"
 	"gopkg.in/tylerb/graceful.v1"
 )
 
@@ -47,6 +51,29 @@ func SetShutdownTimeout(timeout time.Duration) OptionFunc {
 }
 
 func (srv *Server) Run() {
+	// Top-level router.
+	router := mux.NewRouter()
+
+	// Restricted section.
+	secureRouter := mux.NewRouter()
+
+	secure := negroni.New()
+	secure.Use(oauth2.LoginRequired())
+	secure.UseHandler(secureRouter)
+
+	router.Handle("/restricted", secure)
+
+	// Negroni.
 	n := negroni.Classic()
+	n.Use(sessions.Sessions("SalsaFlowSession", cookiestore.New([]byte("SalsaFlow123"))))
+	n.Use(oauth2.Google(&oauth2.Config{
+		ClientID:     "",
+		ClientSecret: "",
+		RedirectURL:  "",
+		Scopes:       []string{},
+	}))
+	n.UseHandler(router)
+
+	// Start the server using graceful.
 	graceful.Run(srv.addr, srv.timeout, n)
 }
